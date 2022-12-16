b5d8ed53b098ecbb74d350a69738ff9393dcf2ac
PC-imatge-2022-2023 <licenses@enti.cat>
2022-12-16 09:13:50 +0100

    fix class bug

--- app/src/main/java/com/example/firstapp/dragging/DragAndDropActivity.kt
+++ app/src/main/java/com/example/firstapp/dragging/DragAndDropActivity.kt
@@ -64,8 +64,8 @@ class DragAndDropActivity : AppCompatActivity() {
         if (data !is Pair<*, *>)
             return@OnDragListener false
 
-        val chip = data.first
-        val star = data.second
+        val chip = data.second
+        val star = data.first
 
         if (chip !is Chip || star !is Star)
             return@OnDragListener false
